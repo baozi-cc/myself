1b3c9b70e7f868135d06a47757e4cf2758ee39c9
baozi-cc <85532857+baozi-cc@users.noreply.github.com>
2023-01-15 21:02:25 +0800

    Update e1.js

--- e1.js
+++ e1.js
@@ -1,4 +1,19 @@
-let html = $response.body;
-html =
-  html.replace(/num":[\d]/g, `num":100`);
-$done({ body: html});
+/*{
+  "message" : "success",
+  "data" : {
+    "ttl" : 0,
+    "message" : ""
+  },
+  "status" : 0
+  直接返回写法,可能要写好几个文件
+}*/
+
+let body = {
+    "message" : "success",
+  "data" : {
+    "ttl" : 0,
+    "message" : ""
+  },
+  "status" : 0
+}
+$done({ body: JSON.stringify(body) });
