9d5018886bb7e9f1517bc51929a834e687c42adc
baozi-cc <85532857+baozi-cc@users.noreply.github.com>
2023-01-15 20:44:22 +0800

    Update e.js

--- e.js
+++ e.js
@@ -1,4 +1,4 @@
-let html = $response.body;
+/*{let html = $response.body;
 
 html =
   html.replace(/num":[\d]/g, `num":100`);
@@ -7,3 +7,14 @@ html =
   html.replace(/ttl":\d+,/g, `ttl":0,`);
 
 $done({ body: html});
+}*/
+
+let body = {
+    "message" : "success",
+  "data" : {
+    "ttl" : 0,
+    "message" : ""
+  },
+  "status" : 0
+}
+$done({ body: JSON.stringify(body) });
