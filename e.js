8e4d755e71860614505994ea9ef7b347474d181c
baozi-cc <85532857+baozi-cc@users.noreply.github.com>
2023-01-15 21:00:17 +0800

    Update e.js

--- e.js
+++ e.js
@@ -1,4 +1,4 @@
-/*{let html = $response.body;
+{let html = $response.body;
 
 html =
   html.replace(/num":[\d]/g, `num":100`);
@@ -7,14 +7,5 @@ html =
   html.replace(/ttl":\d+,/g, `ttl":0,`);
 
 $done({ body: html});
-}*/
-
-let body = {
-    "message" : "success",
-  "data" : {
-    "ttl" : 0,
-    "message" : "活动每周四十点半开始,请耐心等待~"
-  },
-  "status" : 0
 }
-$done({ body: JSON.stringify(body) });
+
