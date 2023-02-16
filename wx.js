69ef7308b0e553c692edbe5e34d6289fc4441d97
baozi-cc <85532857+baozi-cc@users.noreply.github.com>
2023-02-17 00:35:07 +0800

    更新 hjf.js

--- wx.js
+++ wx.js
@@ -1,8 +1,27 @@
-let body = {
-  "errno": "10000",
-  "data": [{
-    "pool_code": 2
-  }],
-  "errmsg": "数据返回成功！"
+
+/**
+
+ */
+
+let html = $response.body;
+
+
+
+html =
+  html.replace(/"receiveState":"2",/g, `"receiveState":"1",`);
+
+html =
+  html.replace(/"rollBeginTime":"\d\d:\d\d:\d\d",/g, `"rollBeginTime":"00:00:00",`);           
+
+html =
+  html.replace(/"cycleStock":0,/g, `"cycleStock":7,`); 
+
+if (html.includes('</head>')) {
+  html = '<script src="https://unpkg.com/vconsole@3.14.6/dist/vconsole.min.js" ignore></script><script ignore>new VConsole()</script>' + html;
 }
-$done({ body: JSON.stringify(body) });
+
+
+$done({ body: html});
+
+$done({ body:`
+`});
